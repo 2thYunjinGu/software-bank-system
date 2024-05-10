242b86dc249e58776050bd5aeba0d1d894b91e30
2thYunjinGu <152866806+2thYunjinGu@users.noreply.github.com>
2024-05-10 13:09:34 +0800

    Update MainView.java

--- View/MainView.java
+++ View/MainView.java
@@ -18,43 +18,55 @@ public class MainView extends JFrame {
     }
 
     private void initializeUI(String username) {
-        setTitle("主页面");
+        setTitle("Main Page");
         setSize(600, 400); // 设置窗口大小
-        setLayout(new FlowLayout()); // 使用流式布局
+        setLayout(null); 
         setLocationRelativeTo(null); // 窗口居中
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel panel_1 = new JPanel(new GridLayout(2, 2));
+        JPanel panel_2 = new JPanel();
+        JPanel panel_3 = new JPanel();
+
+        panel_1.setBounds(0, 60, 600, 200);
+        panel_2.setBounds(0, 300, 600, 50);
+        panel_3.setBounds(0, 0, 600, 50);
+
         // 用户信息显示
-        userInfoLabel = new JLabel("用户名: " + username);
-        add(userInfoLabel);
+        userInfoLabel = new JLabel("Username: " + username);
+        panel_3.add(userInfoLabel);
 
         // 存款按钮
-        depositButton = new JButton("存款");
-        add(depositButton);
+        depositButton = new JButton("Deposit");
+        panel_2.add(depositButton);
 
         // 取款按钮
-        withdrawButton = new JButton("取款");
-        add(withdrawButton);
+        withdrawButton = new JButton("Withdraw");
+        panel_2.add(withdrawButton);
 
         // 交易历史按钮
-        transactionHistoryButton = new JButton("交易记录");
-        add(transactionHistoryButton);
+        transactionHistoryButton = new JButton("Transaction History");
+        panel_2.add(transactionHistoryButton);
 
         // 任务按钮
-        taskButton = new JButton("任务");
-        add(taskButton);
+        taskButton = new JButton("Task");
+        panel_1.add(taskButton);
 
         // 学习按钮
-        studyButton = new JButton("学习");
-        add(studyButton);
+        studyButton = new JButton("Study");
+        panel_1.add(studyButton);
 
         // 商店按钮
-        shopButton = new JButton("商店");
-        add(shopButton);
+        shopButton = new JButton("Shop");
+        panel_1.add(shopButton);
 
         // 监管按钮
-        regulationButton = new JButton("监管");
-        add(regulationButton);
+        regulationButton = new JButton("Regulation");
+        panel_1.add(regulationButton);
+        
+        add(panel_3);
+        add(panel_1);
+        add(panel_2);
     }
 
     public JButton getDepositButton() {
